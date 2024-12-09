3b1becd51086718b602f65d1b7eca3258e2ff0bb
MatteusNogueira <matteus.bins@sptech.school>
2024-12-09 01:34:02 -0300

    alterando link front

--- src/main/java/com/example/CRUD/security/CORS/WebConfig.java
+++ src/main/java/com/example/CRUD/security/CORS/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173"); // Especifique o frontend
-        config.addAllowedOrigin("https://orange-pebble-0a2632b0f.5.azurestaticapps.net"); // Especifique o frontend
+        config.addAllowedOrigin("https://purple-meadow-0d15dc30f.4.azurestaticapps.net"); // Especifique o frontend
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
