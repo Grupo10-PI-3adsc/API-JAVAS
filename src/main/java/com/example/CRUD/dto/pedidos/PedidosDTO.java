25edafd1175054cd71cb089917e9f19a5a7b3696
V1ctorNunes <victor.moreira@sptech.school>
2024-12-10 15:55:18 -0300

    30% pedidos

---
+++ src/main/java/com/example/CRUD/dto/pedidos/PedidosDTO.java
@@ -0,0 +1,18 @@
+package com.example.CRUD.dto.pedidos;
+
+import com.example.CRUD.entity.UserEntity;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Builder
+public class PedidosDTO {
+
+    private Date dataPedido;
+    private Double total;
+    private String status;
+    private String observacoes;
+    private Integer fkUsuario;
+}
